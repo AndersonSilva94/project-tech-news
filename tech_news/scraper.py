52d8b15b5fd919b6f24f0568c6161dc7d052b945
Anderson Silva <andssilva.23@gmail.com>
2022-04-02 00:47:50 -0400

    feat(req 1): :rocket: Crie a função fetch

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,28 @@
+import requests
+import time
+
+
 # Requisito 1
 def fetch(url):
-    """Commit inicial"""
+    """
+    Requisito 1 - passos a se seguir:
+    1 - A função recebe uma url como parâmetro
+    2 - Fazer uma requisição HTTP get utilizando o requests.get
+    3 - Fazer um Rate Limit de 1 req/seg. (Ex: utilizar o time.sleep()
+    4 - Caso a req retorne status 200, pegar o conteúdo de texto
+    5 - Caso retorne um status diferente, retornar None
+    6 - Caso não tenha resposta em até 3seg, a função é abandonada (Timeout)
+        - retornar None
+    """
+    time.sleep(1)
+    try:
+        response_data = requests.get(url)
+        if response_data.status_code != 200:
+            return None
+        else:
+            return response_data.text
+    except requests.Timeout:
+        return None
 
 
 # Requisito 2
