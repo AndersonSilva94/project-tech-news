a20f54034c8cde63626f392d7c9e8b00a5891df7
Anderson Silva <andssilva.23@gmail.com>
2022-04-03 20:15:47 -0400

    feat(req 6): :rocket: Crie a função search_by_title

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,38 @@
+from tech_news.database import search_news
+
+
 # Requisito 6
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    """
+    Requisito 6 - Passos a se seguir:
+    1 - Buscar no banco de dados, notícias por título
+    2 - A função recebe uma string como título de notícia
+    3 - Retornar ao final uma lista de tuplas com as notícias
+        encontradas da seguinte forma:
+        [('titulo', 'url')]
+    4 - A busca deve ser case insensitive
+    5 - Caso não seja encontrada, retornar uma lista vazia
+
+    Lógica a se pensar:
+    1 - Criar uma variável que vai armazenar uma lista com os retornos da
+        busca realizada
+    2 - Para cada valor dessa lista, criar uma tupla de (title, url)
+    3 - Adicionar cada tupla à uma nova lista, que inicialmente é vazia
+    4 - retornar a lista
+
+    OBS: para fazer a busca em case insensitive busquei no link:
+    https://www.mongodb.com/community/forums/t/case-insensitive-search-with-regex/120598/8
+    """
+    data_results = search_news({
+        "title": {
+            "$regex": title,
+            "$options": "i"
+        }
+    })
+    list_tuples = []
+    for news_dict in data_results:
+        list_tuples.append((news_dict["title"], news_dict["url"]))
+    return list_tuples
 
 
 # Requisito 7
