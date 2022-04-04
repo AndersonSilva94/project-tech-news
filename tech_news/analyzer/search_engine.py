32122b81549f2b9ca3f67590b02c8a7352d1c075
Anderson Silva <andssilva.23@gmail.com>
2022-04-03 20:41:31 -0400

    feat(req 7): :rocket: Crie a função search_by_date

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,4 +1,5 @@
 from tech_news.database import search_news
+from datetime import datetime
 
 
 # Requisito 6
@@ -37,7 +38,42 @@ def search_by_title(title):
 
 # Requisito 7
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    """
+    Requisito 7 - Passos a se seguir:
+    1 - Buscar no banco de dados, notícias por data
+    2 - O formato a ser considerado é aaaa-mm-dd
+    3 - Retornar ao final uma lista de tuplas com as notícias
+        encontradas da seguinte forma:
+        [('titulo', 'url')]
+    4 - Caso a data seja inválida ou em formato diferente, retornar
+    um ValueError com a msg 'Data inválida'
+    5 - Caso não seja encontrada, retornar uma lista vazia
+
+    Lógica a se pensar:
+    1- Verificar se a data é válida (usando try, except)
+    2 - Criar uma variável que vai armazenar uma lista com os retornos da
+        busca realizada
+    3 - Para cada valor dessa lista, criar uma tupla de (title, url)
+    4 - Adicionar cada tupla à uma nova lista, que inicialmente é vazia
+    5 - retornar a lista
+
+    OBS: para fazer a verificação da data, busquei no link:
+    https://stackoverflow.com/questions/9978534/match-dates-using-python-regular-expressions/9978701
+    """
+    try:
+        datetime.strptime(date, "%Y-%m-%d")
+    except ValueError:
+        raise ValueError("Data inválida")
+    data_results = search_news({
+        "timestamp": {
+            "$regex": date,
+            "$options": "i"
+        }
+    })
+    list_tuples = []
+    for news_dict in data_results:
+        list_tuples.append((news_dict["title"], news_dict["url"]))
+    return list_tuples
 
 
 # Requisito 8
